dd906ad112510c423ea24326320318858d05f795
Nuriel Shem-Tov <nurielst@hotmail.com>
2019-12-14 23:41:22 +0100

    Typo

--- fullnode_install.sh
+++ fullnode_install.sh
@@ -326,7 +326,7 @@ function set_selections()
 
     RESULTS=$(whiptail --title "Installation Options" --checklist \
         --cancel-button "Exit" \
-        "\nPlease choose additional installation options.\n(Its perfectly okay to leave this as is).\n\
+        "\nPlease choose additional installation options.\n(It is perfectly okay to leave this as is).\n\
 Select/unselect options using space and click Enter to proceed.\n" 28 78 7 \
         "INSTALL_DOCKER"           "Install Docker runtime (recommended)" ON \
         "INSTALL_NGINX"            "Install nginx webserver (recommended)" ON \
