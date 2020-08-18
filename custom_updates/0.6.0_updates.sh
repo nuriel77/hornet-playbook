75f1c0faedd77cacd396eea4b1cc06e462fee732
Nuriel Shem-Tov <nurielst@hotmail.com>
2020-08-18 21:54:00 +0200

    No need for coo address

--- custom_updates/0.6.0_updates.sh
+++ custom_updates/0.6.0_updates.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 set -e
 
-if grep -q UDYXTZBE9GZGPM9SSQV9LTZNDLJIZMPUVVXYXFYVBLIEUHLSEWFTKZZLXYRHHWVQV9MNNX9KZC9D9UZWZ /etc/default/hornet
+if grep -q overwriteCooAddress /etc/default/hornet
 then
     echo "Already updated"
     exit
 fi
 
-sed -i 's|^OPTIONS="\(.*\)"|OPTIONS="\1 --overwriteCooAddress UDYXTZBE9GZGPM9SSQV9LTZNDLJIZMPUVVXYXFYVBLIEUHLSEWFTKZZLXYRHHWVQV9MNNX9KZC9D9UZWZ"|' /etc/default/hornet
+sed -i 's|^OPTIONS="\(.*\)"|OPTIONS="\1 --overwriteCooAddress"|' /etc/default/hornet
 
 echo "Restarting HORNET with new COO address overwrite ..."
 systemctl restart hornet
