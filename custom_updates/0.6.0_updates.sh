d1d1d320b753a93aabab6ebfb88fca8ae7fc13b2
Nuriel Shem-Tov <nurielst@hotmail.com>
2020-08-19 09:29:52 +0200

    Support Centos in latest update

--- custom_updates/0.6.0_updates.sh
+++ custom_updates/0.6.0_updates.sh
@@ -1,13 +1,21 @@
 #!/usr/bin/env bash
 set -e
 
-if grep -q overwriteCooAddress /etc/default/hornet
+if [ -e /etc/default/hornet ]
+then
+    CONFIG_FILE="/etc/default/hornet"
+elif [ -e /etc/sysconfig/hornet ]
+then
+    CONFIG_FILE="/etc/sysconfig/hornet"
+fi
+
+if grep -q overwriteCooAddress "$CONFIG_FILE"
 then
     echo "Already updated"
     exit
 fi
 
-sed -i 's|^OPTIONS="\(.*\)"|OPTIONS="\1 --overwriteCooAddress"|' /etc/default/hornet
+sed -i 's|^OPTIONS="\(.*\)"|OPTIONS="\1 --overwriteCooAddress"|' "$CONFIG_FILE"
 
 echo "Restarting HORNET with new COO address overwrite ..."
 systemctl restart hornet
