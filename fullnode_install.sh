fa504f318302b03053dcbd6a0837f0e5be4227e5
Nuriel Shem-Tov <nurielst@hotmail.com>
2021-04-30 21:21:53 +0200

    Remove tangle monitor/visualizer

--- fullnode_install.sh
+++ fullnode_install.sh
@@ -483,7 +483,7 @@ Please choose additional installation options.
 Note that defaults have been set according to your system's configuration.
 
 Select/unselect options using space and click Enter to proceed.
-        " 24 78 6 \
+        " 24 78 5 \
         "INSTALL_DOCKER"           "Install Docker runtime (recommended)" ON \
         "INSTALL_NGINX"            "Install nginx webserver (recommended)" ON \
         "SKIP_FIREWALL_CONFIG"     "Skip configuring firewall" OFF \
