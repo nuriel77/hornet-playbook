68d8f545a188e2fa6f3e3a6e79f4d3023407c78e
Nuriel Shem-Tov <nurielst@hotmail.com>
2021-05-02 22:22:33 +0200

    Update horc and routes

--- custom_updates/1.0.3_updates.sh
+++ custom_updates/1.0.3_updates.sh
@@ -3,5 +3,3 @@
 cd /opt/hornet-playbook
 git pull origin main
 ansible-playbook -i inventory site.yml -e overwrite=yes --tags=nginx_role -v
-
-read -p "Done updating API restrictions on nginx and added db d/l feature on horc. Please press ENTER to continue."
