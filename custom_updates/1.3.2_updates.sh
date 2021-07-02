c3507b53a36f1f43f9a9220096f43870924c3a82
Nuriel Shem-Tov <nurielst@hotmail.com>
2021-07-02 11:05:06 +0200

    Update updates and values

--- custom_updates/1.3.2_updates.sh
+++ custom_updates/1.3.2_updates.sh
@@ -7,15 +7,22 @@ then
     exit
 fi
 
-TMP_FILE=$(mktemp /tmp/config.json.XXXXXX)
+TMP_FILE_1=$(mktemp /tmp/config.json.XXXXXX)
+TMP_FILE_2=$(mktemp /tmp/config.json.XXXXXX)
 
-jq -r '.protocol.publicKeyRanges[2] |= .+ {"end": 2108160, "key": "ba6d07d1a1aea969e7e435f9f7d1b736ea9e0fcb8de400bf855dba7f2a57e947", "start": 552960}' < /var/lib/hornet/config.json > "$TMP_FILE"
+jq -r '.protocol.publicKeyRanges[2] |= .+ {"end": 2108160, "key": "ba6d07d1a1aea969e7e435f9f7d1b736ea9e0fcb8de400bf855dba7f2a57e947", "start": 552960}' < /var/lib/hornet/config.json > "$TMP_FILE_1"
+jq -r '.warpsync.advancementRange=10000' < "$TMP_FILE_1" > "$TMP_FILE_2"
 
-if jq -e . < "$TMP_FILE" >/dev/null
+if ! jq -e . < "$TMP_FILE_2" >/dev/null
 then
-    mv "$TMP_FILE" /var/lib/hornet/config.json
-    chown hornet:hornet /var/lib/hornet/config.json
-    chmod 600 /var/lib/hornet/config.json
-    echo "Restarting Hornet ..."
-    /bin/systemctl restart hornet
+    echo "Error in /var/lib/hornet/config.json, update failed!"
+    exit 1
 fi
+
+mv "$TMP_FILE_2" /var/lib/hornet/config.json
+chown hornet:hornet /var/lib/hornet/config.json
+chmod 600 /var/lib/hornet/config.json
+rm -f "$TMP_FILE_1"
+
+echo "Restarting Hornet ..."
+/bin/systemctl restart hornet
