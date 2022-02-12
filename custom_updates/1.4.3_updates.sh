bee3f870e55e5282dad688cd45189de16b2c686d
Nuriel Shem-Tov <nurielst@hotmail.com>
2022-02-12 10:42:34 +0100

    vhost config role ref fix

--- custom_updates/1.4.3_updates.sh
+++ custom_updates/1.4.3_updates.sh
@@ -2,6 +2,6 @@
 
 cd /opt/hornet-playbook
 git pull origin main
-ansible-playbook -i inventory site.yml -e overwrite=yes --tags=nginx_role -v
+ansible-playbook -i inventory site.yml -e overwrite=yes --tags=vhosts_config -v
 
 read -p "Done updating MQTT nginx configuration. Please press ENTER to continue."
