89f6cbdf6fa5b2e88a53778a02ce18d72f7d8d28
Nuriel Shem-Tov <nurielst@hotmail.com>
2022-04-13 20:42:42 +0200

    Update custom script 1.4.4

--- custom_updates/1.4.4_updates.sh
+++ custom_updates/1.4.4_updates.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env bash
+set -e
 
 cd /opt/hornet-playbook
 git pull origin main
+
+echo "Stopping hornet..."
+/bin/systemctl stop hornet || true
+
+echo "Deleting participation DB..."
+rm -rf /var/lib/hornet/mainnetdb/participation
 ansible-playbook -i inventory site.yml -e overwrite=yes --tags=hornet_config_file -v
 
+echo "Ensure hornet is started..."
+/bin/systemctl start hornet || true
 read -p "Done updating Hornet configuration file. Please press ENTER to continue."
